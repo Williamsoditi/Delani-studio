517bd5882c47250a33f643ce13935eae604baba3
Williamsoditi <williams.oditi@student.moringaschool.com>
2022-03-11 11:27:48 +0300

    Feat : Optimised footer date - This optimises the date to an infinite year

--- js/script.js
+++ js/script.js
@@ -2,15 +2,20 @@ $(document).ready(function () {
     $(".design-logo").click(function () {
         $("#design").fadeToggle();
     });
-    $(".development-logo").click(function name(params) {
+    $(".development-logo").click(function name() {
         $("#development").fadeToggle();
     });
-    $(".product-management").click(function name(params) {
+    $(".product-management").click(function name() {
         $("#management").fadeToggle();
     });
+    $("#img-1").hover(function () {
+        $(".img-1").fadeToggle();
+    });
 
-    
 })
-function mySubmit() {
-    alert("Thank you for reaching out!");
-}
+let date = new Date().getFullYear();
+let dateTag = document.getElementById("date");
+dateTag.innerHTML = date;
+// function mySubmit() {
+//     alert("Thank you for reaching out!");
+// }
