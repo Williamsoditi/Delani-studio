80852e0b85b5c3d68a1b24e789545b1afbdcbfde
Williamsoditi <williams.oditi@student.moringaschool.com>
2022-03-11 06:44:23 +0300

    Feat : Optimized what we do section - This implements jQuery logic to the section making it dynamic

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $(".design-logo").click(function () {
+        $("#design").fadeToggle();
+    });
+    $(".development-logo").click(function name(params) {
+        $("#development").fadeToggle();
+    });
+    $(".product-management").click(function name(params) {
+        $("#management").fadeToggle();
+    });
+})
